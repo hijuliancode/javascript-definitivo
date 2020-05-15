a3b94988f513cc6d63400b35e76c1c12eb40fe8a
JulianCode <hijuliansosa@protonmail.com>
2020-05-15 11:38:42 -0500

    traversing

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
-// Eliminar de Local Storage
-localStorage.clear();
+localStorage.clear(); // Eliminar de Local Storage
+
+// Traversing: Navegar a través de todo nuestro elemento
+const cursos = document.querySelectorAll('.card')
+
+console.log(cursos[0])
+console.log(cursos[0].lastChild)
+console.log(cursos[0].lastElementChild)
+console.log(cursos[0].childElementCount)
+
+console.log('--- --- ---')
+
+let enlaces = document.querySelectorAll('a');
+console.log(enlaces[0].parentElement.parentElement.parentElement.parentElement)
+
+console.log('--- --- ---')
+console.log(cursos[0].parentElement.parentElement.parentElement.children[0].textContent = 'Nuevo Encabezado desde Traversing')
+
+console.log('--- --- ---')
+enlaces = document.querySelectorAll('.enlace');
+console.log(enlaces[0].nextElementSibling.nextElementSibling.parentElement.children[4])
+
+// NodeTypes
+// 1 - Elementos
+// 2 - Atributos
+// 3 - TextNode
+// 8 - Comentarios
+// 9 - Documentos
+// 10 - Doctype
