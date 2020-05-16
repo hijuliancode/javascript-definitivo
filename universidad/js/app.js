82bd1bd15ceccdecb97f04ea6822483e7a48477f
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 18:37:04 -0500

    Event Listeners: Mouse events

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,2 +1,29 @@
 // Eliminar de Local Storage
 localStorage.clear();
+
+// Variables
+const encabezado = document.querySelector('#encabezado')
+const enlaces = document.querySelectorAll('.enlace')
+const boton = document.querySelector('#vaciar-carrito')
+
+// Click
+// boton.addEventListener('click', obtenerEvento)
+// Double Click
+// boton.addEventListener('dblclick', obtenerEvento)
+// Mouse enter
+// boton.addEventListener('mouseenter', obtenerEvento)
+// // Mouse leave
+// boton.addEventListener('mouseleave', obtenerEvento)
+// // Mouse over
+// boton.addEventListener('mouseover', obtenerEvento)
+// // Mouse out
+// boton.addEventListener('mouseout', obtenerEvento)
+// // Mouse down
+boton.addEventListener('mousedown', obtenerEvento)
+// // Mouse up
+boton.addEventListener('mouseup', obtenerEvento)
+
+function obtenerEvento(e) {
+  e.preventDefault()
+  console.log(`Evento: ${e.type}`);
+}
