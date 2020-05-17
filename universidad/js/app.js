51ae039cf9c46ee8795556d4b3d1d69fba9f949b
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 19:25:28 -0500

    Event Bubbling : comment

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -2,6 +2,11 @@
 localStorage.clear();
 
 // Event Bubbling
+// Sucede cuando elementos, dentro de otro elemento tienen funciones, al dar click a un elemento
+// dentro de otro se ejecutan ambas funcionas, ejemplo, un botón dentro de un card, al dar click
+// en el botón solo el botón debería ejecutar su función, pero además del botón la ejecutan sus
+// parents, que también tenían funciones a ejecutar en caso de click.
+
 const card = document.querySelector('.card')
 const infoCard = document.querySelector('.info-card')
 const agregrarCarrito = document.querySelector('.agregar-carrito')
