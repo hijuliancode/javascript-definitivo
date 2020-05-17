8501879eed32e2703c693dc9ad320e38c79cd76c
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 22:40:33 -0500

    LocalStorage

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,2 +1,9 @@
-// Eliminar de Local Storage
-localStorage.clear();
+// LocalStorage
+localStorage.setItem('nombre0', 'Valentina') // Agregar
+// localStorage.removeItem('nombre0') // Eliminar
+const nombre = localStorage.getItem('nombre0') // Traer
+console.log(nombre)
+localStorage.clear() // eliminar todo el LS
+
+// Session Storage
+// sessionStorage.setItem('nombre1', 'Julian')
