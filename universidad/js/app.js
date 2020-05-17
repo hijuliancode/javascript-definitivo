9dddd2e9f126bf01a3daee1e93276892afc15c7c
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 22:30:36 -0500

    Delegation en JS

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,2 +1,16 @@
 // Eliminar de Local Storage
 localStorage.clear();
+
+// Delegation
+document.body.addEventListener('click', eliminarElemento)
+
+function eliminarElemento(e) {
+  e.preventDefault()
+
+  if(e.target.classList.contains('borrar-curso')) {
+    console.log(e.target.parentElement.parentElement.remove())
+  }
+  if(e.target.classList.contains('agregar-carrito')) {
+    console.log('Agregado al carrito!')
+  }
+}
