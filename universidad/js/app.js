78cdd6840dc7c1d4c4bdbf07a0f425f90c2c2f55
JulianCode <hijuliansosa@protonmail.com>
2020-05-16 19:18:20 -0500

    Event Bubbling

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,2 +1,21 @@
 // Eliminar de Local Storage
 localStorage.clear();
+
+// Event Bubbling
+const card = document.querySelector('.card')
+const infoCard = document.querySelector('.info-card')
+const agregrarCarrito = document.querySelector('.agregar-carrito')
+
+card.addEventListener('click', e => {
+  console.log('click en card')
+  e.stopPropagation()
+})
+infoCard.addEventListener('click', e => {
+  console.log('click en infoCard')
+  e.stopPropagation()
+})
+agregrarCarrito.addEventListener('click', e => {
+  e.preventDefault()
+  console.log('click en agregrarCarrito')
+  e.stopPropagation()
+})
