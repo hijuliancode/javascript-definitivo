afcbebb4fa8a5d9e7667d1829103f385dfe9b176
JulianCode <hijuliansosa@protonmail.com>
2020-05-19 02:50:57 -0500

    Heredar Prototypes a otro Objeto

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -3,20 +3,20 @@ function Cliente(nombre, saldo) {
   this.saldo = saldo;
 }
 
-Cliente.prototype.tipoCliente = function () {
-  let tipo;
-  if(this.saldo > 1000) {
-    tipo = 'gold';
-  } else if(this.saldo > 500) {
-    tipo = 'platinum';
-  } else {
-    tipo = 'normal';
-  }
-  return tipo;
+Cliente.prototype.nombreClienteSaldo = function () {
+  return `Nombre: ${this.nombre}, tu saldo es ${this.saldo}`
 }
 
+function Empresa(nombre, saldo, telefono, tipo) {
+  Cliente.call(this, nombre, saldo); // Heredamos las propiedades
+  this.telefono = telefono;
+  this.tipo = tipo;
+}
+// Además debemos heredar el prototipo
+Empresa.prototype = Object.create(Cliente.prototype);
+
 const Persona1 = new Cliente('Pedro', 20000);
-const Persona2 = new Cliente('Karen', 6000);
+const Empresa1 = new Empresa('Kodak', 6000, 1231231, "Fotografía");
 
 console.log(Persona1)
-console.log(Persona2)
+console.log(Empresa1)
