488ca0e80fc18402c2b355d52b5414fadf290a31
JulianCode <hijuliansosa@protonmail.com>
2020-05-19 02:59:48 -0500

    La función Object Create

--- universidad/js/app.js
+++ universidad/js/app.js
@@ -1,22 +1,16 @@
-function Cliente(nombre, saldo) {
-  this.nombre = nombre;
-  this.saldo = saldo;
+const Cliente = {
+  imprimirSaldo: function() {
+    return `Hola ${this.nombre}, tu saldo es ${this.saldo}`;
+  },
+  retirarSaldo: function(retiro) {
+    return this.saldo -= retiro;
+  }
 }
+// Crear el objeto
+const vale = Object.create(Cliente);
+vale.nombre = 'Valentina';
+vale.saldo = 200000;
 
-Cliente.prototype.nombreClienteSaldo = function () {
-  return `Nombre: ${this.nombre}, tu saldo es ${this.saldo}`
-}
+vale.retirarSaldo(200);
 
-function Empresa(nombre, saldo, telefono, tipo) {
-  Cliente.call(this, nombre, saldo); // Heredamos las propiedades
-  this.telefono = telefono;
-  this.tipo = tipo;
-}
-// Además debemos heredar el prototipo
-Empresa.prototype = Object.create(Cliente.prototype);
-
-const Persona1 = new Cliente('Pedro', 20000);
-const Empresa1 = new Empresa('Kodak', 6000, 1231231, "Fotografía");
-
-console.log(Persona1)
-console.log(Empresa1)
+console.log(vale.imprimirSaldo())
