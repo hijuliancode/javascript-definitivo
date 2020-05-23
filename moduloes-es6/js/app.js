95fe6c2b89d2346eb3af6cf3926a1646f9750694
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 12:01:32 -0500

    How to create JavaScript modules

---
+++ moduloes-es6/js/app.js
@@ -0,0 +1,6 @@
+// import * as cliente from "./cliente.js"; // En caso de muchas importancioens
+// console.log(cliente.nombreCliente)
+
+import { nombreCliente, ahorro } from "./cliente.js";
+
+console.log(nombreCliente)
---
+++ moduloes-es6/js/cliente.js
@@ -0,0 +1,6 @@
+export const nombreCliente = 'Pedro'
+export const ahorro = 2000
+
+function mostrarInformacion() {
+  return `Nombre: ${nombreCliente} - Ahorro: ${ahorro}`
+}
