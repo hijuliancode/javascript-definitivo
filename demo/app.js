19a3e3bfc492a56fc335b860103f28696e87cbb9
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 23:49:53 -0500

    Iterators

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,31 @@
-// Variables
+// Iterators
+// Ofrecen un poco más de flexibilidad y control sobre la ejecución del código.
+// La ventaja principal de utilizar un iterador es que tienes control total en cada iteración
 
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
+// Un iterador que vaya paso a paso
+function crearIterador(carrito) {
+  // Inicializar el indice
+  let i = 0
+  return {
+    siguiente: () => {
+      let fin = (i >= carrito.length)
 
-// Functions
-function init() {
-  console.log('demo app')
+      // Leer el valor siempre y cuando no hayamos llegado al fin (al final del carrito)
+      let valor = !fin ? carrito[i++] : undefined
+
+      return {
+        fin: fin,
+        valor: valor
+      }
+    }
+  }
 }
+
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4']
+const recorrerCarrito = crearIterador(carrito)
+
+console.log(recorrerCarrito.siguiente())
+console.log(recorrerCarrito.siguiente())
+console.log(recorrerCarrito.siguiente())
+console.log(recorrerCarrito.siguiente())
+console.log(recorrerCarrito.siguiente())
