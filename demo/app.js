c41708a0940c4572f26297c632d79434dd2ef2ed
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 21:22:22 -0500

    Destructuing arrays and objects

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,23 @@
-// Variables
-
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
-
-// Functions
-function init() {
-  console.log('demo app')
+const cliente  = {
+  tipo: 'Premium',
+  saldo: 3000,
+  datos: {
+    nombre: 'Meraki',
+    apellido: 'SC',
+    residencia: {
+      ciudad: 'Sibaté'
+    }
+  },
+  movimientos: ['12-03-2018', '12-03-2017', '12-03-2016']
 }
+
+let {
+  tipo,
+  datos: {residencia},
+  movimientos: [primerMovimiento],
+  movimientos: [ , , ultimoMovimiento]
+} = cliente;
+console.log(tipo);
+console.log(residencia);
+console.log(primerMovimiento);
+console.log(ultimoMovimiento);
