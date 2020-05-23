1cbcfd365275207a2b2975715359905e07008b61
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 13:10:15 -0500

    Export / Import from multiple files && alias

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -1,15 +1,14 @@
 // import * as cliente from "./cliente.js"; // En caso de muchas importancioens
 // console.log(cliente.nombreCliente)
 
-import { nombreCliente, ahorro, mostrarInformacion, Cliente } from "./cliente.js";
+import { nombreCliente, ahorro, mostrarInformacion, Cliente } from "./cliente.js"
+import { nombreEmpresa, ahorro as ahorroEmpresa, mostrarInformacion as infoEmpresa, categoria } from "./empresa.js"
 
-// console.log(mostrarInformacion(nombreCliente, ahorro))
+console.log(nombreCliente)
+console.log(nombreEmpresa)
+console.log(ahorro)
+console.log(ahorroEmpresa)
 
 
-
-// Utilizando la clase importada
-
-const cliente = new Cliente('Meraki', 200000)
-console.log(cliente)
-console.log(cliente.nombre)
-console.log(cliente.ahorro)
+console.log(mostrarInformacion(nombreCliente, ahorro))
+console.log(infoEmpresa(nombreEmpresa, ahorroEmpresa, categoria))
---
+++ moduloes-es6/js/empresa.js
@@ -0,0 +1,9 @@
+// Exportar Variables
+export const nombreEmpresa = 'Udemy'
+export const ahorro = 2000000000
+export const categoria = 'Aprendizaje'
+
+// Exportar Funciones
+export function mostrarInformacion(a, b, c) {
+  return `Nombre: ${a} - Ahorro: ${b} - Categoría: ${c}`
+}
