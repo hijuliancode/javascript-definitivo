013443f305c8fb2ac472209c6cf682ffba789ae2
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 22:11:47 -0500

    Symbol

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,34 @@
-// Variables
+// Symbols
 
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
+const simbolo = Symbol()
+const simbolo2 = Symbol('Descripción aquí')
 
-// Functions
-function init() {
-  console.log('demo app')
+console.log(simbolo)
+console.log(simbolo2)
+
+// Cada symbol es diferente de otro, son únicos en la ejecución de javascript
+console.log(Symbol() === Symbol()) // false
+
+
+///////
+
+let nombre = Symbol()
+let apellido = Symbol()
+
+// Crear una persona
+let persona = {}
+persona.nombre = 'Ginna'
+persona[nombre] = 'Meraki' // Para asignar Symbol debe ser con con llaves cuadradas []
+persona[apellido] = 'SC'
+persona.saldo = 1000
+persona.tipoCliente = 'Gold'
+
+console.log(persona) // Los symbols se muestran en el objeto
+console.log(persona.nombre) // Pero no se acceden con punto (.) los symbols,
+console.log(persona[apellido]) // deben ser con []
+
+// Se consideran como si fueran propiedades privadas
+// Y al iterar el objeto los symbols no se mostrarán
+for(let i in persona) {
+  console.log(`${i}: ${persona[i]}`) // No se imprimen los symbols
 }
