87a1105b7ec3f0cfb01b1984087840dea2f9a2b9
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 13:02:24 -0500

    Export / Import classes in modules

--- moduloes-es6/js/app.js
+++ moduloes-es6/js/app.js
@@ -1,9 +1,15 @@
 // import * as cliente from "./cliente.js"; // En caso de muchas importancioens
 // console.log(cliente.nombreCliente)
 
-import { nombreCliente, ahorro, mostrarInformacion, mostrarNombre } from "./cliente.js";
+import { nombreCliente, ahorro, mostrarInformacion, Cliente } from "./cliente.js";
 
-console.log(mostrarInformacion(nombreCliente, ahorro))
+// console.log(mostrarInformacion(nombreCliente, ahorro))
 
-const mostrarNombreCliente = mostrarNombre(nombreCliente);
-console.log(mostrarNombreCliente)
+
+
+// Utilizando la clase importada
+
+const cliente = new Cliente('Meraki', 200000)
+console.log(cliente)
+console.log(cliente.nombre)
+console.log(cliente.ahorro)
--- moduloes-es6/js/cliente.js
+++ moduloes-es6/js/cliente.js
@@ -1,10 +1,17 @@
+// Exportar Variables
 export const nombreCliente = 'Pedro'
 export const ahorro = 2000
 
+// Exportar Funciones
 export function mostrarInformacion() {
   return `Nombre: ${nombreCliente} - Ahorro: ${ahorro}`
 }
 
-export function mostrarNombre(nombre)  {
-  return `Nombre del Cliente: ${nombre}`
+// Exportar Clase
+
+export class Cliente {
+  constructor(nombre, ahorro) {
+    this.nombre = nombre,
+    this.ahorro = ahorro
+  }
 }
