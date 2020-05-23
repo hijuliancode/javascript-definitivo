15769016126daf624dec4509f5340754dc0c888e
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 21:44:25 -0500

    Destructuring to Functions

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,36 @@
-// Variables
+function reservacion(completo, opciones) {
+  // Destructuring forma anterior
+  // opciones = opciones || {};
+  // let metodo = opciones.metodoDePago,
+  //     cantidad = opciones.cantidad,
+  //     dias = opciones.dias;
 
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
+  // Destructuring forma nueva
+  let { metodoDePago, cantidad, dias } = opciones;
 
-// Functions
-function init() {
-  console.log('demo app')
+  console.log(metodoDePago)
+  console.log(cantidad)
+  console.log(dias)
 }
+
+reservacion(
+  true,
+  {
+    metodoDePago: 'Tarjeta',
+    cantidad: 2000,
+    dias: 3
+  }
+)
+console.log('---')
+// Parametros por defecto de la función
+function reservacion2(completo, { // Este sigue siendo un parametro
+      metodoDePago = 'Efectivo',
+      cantidad = 0,
+      dias = 0
+    } = {}
+  ) { // El cuerpo de la función
+    console.log(metodoDePago)
+    console.log(cantidad)
+    console.log(dias)
+}
+reservacion2(false)
