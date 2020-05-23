b8930644f0bae8345464674bd35972f42ce7c0e1
JulianCode <hijuliansosa@protonmail.com>
2020-05-22 20:48:02 -0500

    Destructuring allocation

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,22 @@
-// Variables
-
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
-
-// Functions
-function init() {
-  console.log('demo app')
+// Asignación por destructuring
+const cliente = {
+  tipo: 'Premium',
+  nombre: 'Ginna',
+  datos: {
+    ubicacion: {
+      ciudad: 'Bogotá',
+      pais: 'Colombia'
+    },
+    cuenta: {
+      desde: '10-12-2012',
+      saldo: 4000
+    }
+  }
 }
+
+let { nombre, datos, datos: { cuenta, ubicacion } } = cliente;
+
+console.log(nombre)
+console.log(datos)
+console.log(cuenta)
+console.log(ubicacion)
