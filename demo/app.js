726d9f783a61e7d75a905022588cd65a8320bb46
JulianCode <hijuliansosa@protonmail.com>
2020-05-23 01:32:01 -0500

    generators

--- demo/app.js
+++ demo/app.js
@@ -1,9 +1,24 @@
-// Variables
-
-// Event Listeners
-document.addEventListener('DOMContentLoaded', init)
-
-// Functions
-function init() {
-  console.log('demo app')
+// Generators
+function *crearGenerador() {
+  yield 1
+  yield 'Nombre'
+  yield 3 + 3
+  yield true
 }
+
+// const iterador = crearGenerador()
+// console.log(iterador.next().value)
+// console.log(iterador.next().value)
+// console.log(iterador.next().value)
+
+function *nuevoGenerador(carrito) {
+  for(let i = 0; i < carrito.length; i++ ) {
+    yield carrito[i]
+  }
+}
+
+const carrito = ['Producto 1', 'Producto 2', 'Producto 3', 'Producto 4'];
+const iterador = nuevoGenerador(carrito);
+
+console.log(iterador.next().value)
+console.log(iterador.next().value)
